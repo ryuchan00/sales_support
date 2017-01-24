26c4de5ca5c48f825366782d6795b09b9e17e31a
yamakawa00 <leokun0210@gmail.com>
2017-01-24 19:23:09 +0900

    update

--- Model/PDO.php
+++ Model/PDO.php
@@ -2,7 +2,7 @@
 
 class Connect
 {
-    public function pdo()
+    function pdo()
     {
         $url = parse_url(getenv('DATABASE_URL'));
         $dsn = sprintf('pgsql:host=%s;dbname=%s;charset=utf8;', $url['host'], substr($url['path'], 1));
