3bd3701ee44d32a34487c96f216501896b62b8f4
yamakawa00 <leokun0210@gmail.com>
2017-01-24 22:56:21 +0900

    update

--- Controller/index.php
+++ Controller/index.php
@@ -80,8 +80,8 @@ foreach ($events as $event) {
     $target_hh = array("9","10","11","12","13","14","15","16","17","18","19","20","21","22");
     $target_mm = array("00","15","30","45");
     $columnArray = array();
+    $actionArray = array();
     foreach ($target_hh as $k => $v) {
-        $actionArray = array();
         array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
             $v));
         if (($k + 1) % 3 == 0) {
@@ -92,6 +92,7 @@ foreach ($events as $event) {
                 $actionArray
             );
             array_push($columnArray, $column);
+            $actionArray = array();
         }
     }
     if (($k + 1) % 3 != 0) {
