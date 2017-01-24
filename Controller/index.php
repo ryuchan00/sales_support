fc34e96a909bd7856cd3eebeda57b41d942b3eb3
yamakawa00 <leokun0210@gmail.com>
2017-01-24 22:46:50 +0900

    update

--- Controller/index.php
+++ Controller/index.php
@@ -80,26 +80,37 @@ foreach ($events as $event) {
     $target_hh = array("9","10","11","12","13","14","15","16","17","18","19","20","21","22");
     $target_mm = array("00","15","30","45");
     $columnArray = array();
-    // foreach ($target_hh) {
-    //
-    // }
-    for($i = 0; $i < 5; $i++) {
+    foreach ($target_hh as $k => $v) {
         $actionArray = array();
         array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-            "ボタン" . $i . "-" . 1, "c-" . $i . "-" . 1));
-        array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-            "ボタン" . $i . "-" . 2, "c-" . $i . "-" . 2));
-        array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-            "ボタン" . $i . "-" . 3, "c-" . $i . "-" . 3));
-        array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-            "ボタン" . $i . "-" . 4, "c-" . $i . "-" . 4));
-        $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
-            ($i + 1) . "日後の天気",
-            "晴れ",
-            "https://" . $_SERVER["HTTP_HOST"] .  "/imgs/template.jpg",
-            $actionArray
-        );
+            $v));
+        if (($k + 1) % 3 == 0) {
+            $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
+                "帰社時間選択",
+                "何時に帰社しますか?",
+                "https://" . $_SERVER["HTTP_HOST"] .  "/imgs/template.jpg",
+                $actionArray
+            );
+        }
         array_push($columnArray, $column);
     }
-    replyCarouselTemplate($bot, $event->getReplyToken(),"今後の天気予報", $columnArray);
+    replyCarouselTemplate($bot, $event->getReplyToken(),"帰社報告", $columnArray);
+
+    // for($i = 0; $i < 5; $i++) {
+    //     $actionArray = array();
+    //     array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
+    //         "ボタン" . $i . "-" . 1, "c-" . $i . "-" . 1));
+    //     array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
+    //         "ボタン" . $i . "-" . 2, "c-" . $i . "-" . 2));
+    //     array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
+    //         "ボタン" . $i . "-" . 3, "c-" . $i . "-" . 3));
+    //     $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
+    //         ($i + 1) . "日後の天気",
+    //         "晴れ",
+    //         "https://" . $_SERVER["HTTP_HOST"] .  "/imgs/template.jpg",
+    //         $actionArray
+    //     );
+    //     array_push($columnArray, $column);
+    // }
+    // replyCarouselTemplate($bot, $event->getReplyToken(),"今後の天気予報", $columnArray);
 }
