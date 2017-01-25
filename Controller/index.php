51f61131c5a98d47ca9ac8e55d162ed1d1ce28ed
yamakawa00 <leokun0210@gmail.com>
2017-01-25 13:16:11 +0900

    update

--- Controller/index.php
+++ Controller/index.php
@@ -79,7 +79,8 @@ foreach ($events as $event) {
 
     // 帰社処理
     // am 9:00 ~ pm 22:45
-    $target_hh = array("9","10","11","12","13","14","15","16","17","18","19","20","21","22");
+    $target_hh = array("9","10");
+    // $target_hh = array("9","10","11","12","13","14","15","16","17","18","19","20","21","22");
     $target_mm = array("00","15","30","45");
     $columnArray = array();
     $actionArray = array();
