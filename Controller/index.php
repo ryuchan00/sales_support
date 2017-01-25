9b2a0f5a651b58bdcc2a502ce745b25fb13b7927
yamakawa00 <leokun0210@gmail.com>
2017-01-25 09:18:47 +0900

    update

--- Controller/index.php
+++ Controller/index.php
@@ -79,11 +79,11 @@ foreach ($events as $event) {
     // am 9:00 ~ pm 22:45
     $target_hh = array("9","10","11","12","13","14","15","16","17","18","19","20","21","22");
     $target_mm = array("00","15","30","45");
-    $columnArray = array();
-    $actionArray = array();
+    $columnArray = [];
+    $actionArray = [];
     foreach ($target_hh as $k => $v) {
         array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-            $v));
+            "a"));
         if (($k + 1) % 3 == 0) {
             $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
                 "帰社時間選択",
