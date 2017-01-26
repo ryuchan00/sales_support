a88d537f5d1020c647e3469e66618dc95ec43cd7
yamakawa00 <leokun0210@gmail.com>
2017-01-26 21:53:12 +0900

    update

--- Controller/index.php
+++ Controller/index.php
@@ -43,7 +43,9 @@ foreach ($events as $event) {
     $pdo = new Connect;
     $sql = "SELECT * FROM public.user WHERE user_line_id=\':user_line_id\'";
     $items=$pdo->plural($sql,$profile["userId"]);
-    error_log($items['user_line_id']);
+    foreach ($items as $v) {
+        error_log($v['user_line_id']);
+    }
     $pdo->registerProfile($profile);
     // $sql = 'insert into public.user (user_line_id, name, comment, picture_url) values (:user_line_id, :name, :comment, :picture_url)';
     // $stmt = $pdo->pdo()->prepare($sql);
