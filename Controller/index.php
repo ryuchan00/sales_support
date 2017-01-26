11a9b913df80f82099fe9b8de91eead9765dcc0f
yamakawa00 <leokun0210@gmail.com>
2017-01-26 09:23:35 +0900

    local commit

--- Controller/index.php
+++ Controller/index.php
@@ -83,15 +83,15 @@ foreach ($events as $event) {
     // $target_hh = array("9","10","11","12","13","14");
     $target_hh = array("9","10","11","12","13","14","15","16","17","18","19","20","21","22","23");
     $target_mm = array("00","15","30","45");
-    $columnArray = array();
-    $actionArray = array();
+    $columnArray = [];
+    $actionArray = [];
     foreach ($target_hh as $k => $v) {
         array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
             $v, $v));
         error_log("process 1");
         if ((($k + 1) % 3 == 0) || (($k + 1) == (count($target_hh)))) {
             $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
-                "帰社時間選択",
+                "時選択",
                 "何時に帰社しますか?",
                 "https://" . $_SERVER["HTTP_HOST"] .  "/imgs/template.png",
                 $actionArray
