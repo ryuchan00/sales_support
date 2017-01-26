2f74d8fafaa7f1408f1793cb26920c466b506467
yamakawa00 <leokun0210@gmail.com>
2017-01-26 22:10:45 +0900

    update

--- Model/Connect.php
+++ Model/Connect.php
@@ -20,29 +20,29 @@ class Connect
 
     function registerProfile($profile)
     {
-        $sql = "SELECT * FROM public.user WHERE user_line_id=':id'";
-        // $hoge = $this->pdo();
-        $items = $this->plural($sql, $profile["userId"]);
-        error_log($profile["userId"]);
-        if (empty($items['user_line_id'])) {
-            error_log("throw empty");
-            $sql = 'insert into public.user (user_line_id, name, comment, picture_url) values (:user_line_id, :name, :comment, :picture_url)';
-            $stmt = $this->pdo()->prepare($sql);
-            $stmt->bindValue(":user_line_id", $profile["userId"]);
-            $stmt->bindValue(":name", $profile["displayName"]);
-            $stmt->bindValue(":comment", $profile["statusMessage"]);
-            $stmt->bindValue(":picture_url", $profile["pictureUrl"]);
-            $flag = $stmt->execute();
-            if ($flag){
-               error_log('データの追加に成功しました');
-            }else{
-               error_log('データの追加に失敗しました');
-            }
-        } else {
-            error_log("throw not empty");
-        }
-        error_log("end of method");
-    }
+    //     $sql = "SELECT * FROM public.user WHERE user_line_id=':id'";
+    //     // $hoge = $this->pdo();
+    //     $items = $this->plural($sql, $profile["userId"]);
+    //     error_log($profile["userId"]);
+    //     if (empty($items['user_line_id'])) {
+    //         error_log("throw empty");
+    //         $sql = 'insert into public.user (user_line_id, name, comment, picture_url) values (:user_line_id, :name, :comment, :picture_url)';
+    //         $stmt = $this->pdo()->prepare($sql);
+    //         $stmt->bindValue(":user_line_id", $profile["userId"]);
+    //         $stmt->bindValue(":name", $profile["displayName"]);
+    //         $stmt->bindValue(":comment", $profile["statusMessage"]);
+    //         $stmt->bindValue(":picture_url", $profile["pictureUrl"]);
+    //         $flag = $stmt->execute();
+    //         if ($flag){
+    //            error_log('データの追加に成功しました');
+    //         }else{
+    //            error_log('データの追加に失敗しました');
+    //         }
+    //     } else {
+    //         error_log("throw not empty");
+    //     }
+    //     error_log("end of method");
+    // }
 
     //SELECT文のときに使用する関数。
     function select($sql)
@@ -58,7 +58,8 @@ class Connect
         error_log('item'.$item);
         $hoge=$this->pdo();
         $stmt=$hoge->prepare($sql);
-        $stmt->execute(array(':id'=>$item));//sql文のVALUES等の値が?の場合は$itemでもいい。
+        // $stmt->execute(array(':id'=>$item));//sql文のVALUES等の値が?の場合は$itemでもいい。
+        $stmt->execute(array($item));
         error_log($stmt->debugDumpParams());
         return $stmt;
     }
