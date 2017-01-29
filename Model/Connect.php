f75afa0982c543779c9be77f4dffcf180d7096ad
yamakawa00 <leokun0210@gmail.com>
2017-01-29 21:51:35 +0900

    update

--- Model/Connect.php
+++ Model/Connect.php
@@ -22,8 +22,8 @@ class Connect
     {
         // $sql = "SELECT user_line_id, name FROM public.user WHERE user_line_id=:id";
         $sql = "select user_line_id, name from public.user where user_line_id=:id";
-        $stmt->bindValue(":id", $profile["userId"]);
         $stmt = $this->pdo()->query($sql);
+        $stmt->bindValue(":id", $profile["userId"]);
         while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
             error_log($result['user_line_id']);
             error_log($result['name']);
