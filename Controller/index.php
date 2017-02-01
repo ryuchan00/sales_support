3c993d87dce838ea0621436c53e5c802b2ed5830
yamakawa00 <leokun0210@gmail.com>
2017-02-01 09:25:16 +0900

    update

--- Controller/index.php
+++ Controller/index.php
@@ -68,13 +68,13 @@ foreach ($events as $event) {
 
     // 友達追加処理
 
+    // am 9:00 ~ pm 22:45
     $target_hh = array("9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23");
     $target_mm = array("00", "15", "30", "45");
     if (($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
         $post_msg = $event->getText();
         switch ($post_msg) {
             case "帰社":
-                // am 9:00 ~ pm 22:45
                 $columnArray = [];
                 $actionArray = [];
                 foreach ($target_hh as $k => $v) {
